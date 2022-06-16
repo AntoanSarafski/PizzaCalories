5371c193163ac5e5583a8c98f44ebfce468df4a1
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-06-17 01:53:04 +0300

    Final Solution
    :)

--- PizzaCalories/PizzaCalories/StartUp.cs
+++ PizzaCalories/PizzaCalories/StartUp.cs
@@ -6,15 +6,42 @@ namespace PizzaCalories
     {
         static void Main(string[] args)
         {
-            string[] input = Console.ReadLine().Split();
-            string type = input[0];
-            string flourType = input[1];
-            //string bakingTechnique = input[2];
-            int weight = int.Parse(input[2]);
 
-            Topping dough = new Topping(flourType, weight);
+            string[] pizzaInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name = pizzaInfo[1];
 
-            Console.WriteLine(dough.Calories);
+            string[] doughInfo = Console.ReadLine().Split();
+
+            string flourType = doughInfo[1];
+            string bakingTechnique = doughInfo[2];
+            int weight = int.Parse(doughInfo[3]);
+
+            try
+            {
+                Dough dough = new Dough(flourType, bakingTechnique, weight);
+
+                Pizza pizza = new Pizza(name, dough);
+
+                string input = Console.ReadLine();
+                while (input != "END")
+                {
+                    string[] toppingInfo = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    string toppingType = toppingInfo[1];
+                    int toppingWeight = int.Parse(toppingInfo[2]);
+
+                    Topping topping = new Topping(toppingType, toppingWeight);
+
+                    pizza.AddTopping(topping);
+
+                    input = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{pizza.Name} - {pizza.Calories:F2} Calories.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
         }
     }
